ab080db963a06c6028134512b2b8b4a659425d26
Tami Churns <tchurns@paperlesspost.com>
2019-04-03 22:46:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books').then((booksData) => {
+    return booksData.json()
+  }).then((booksJson) => renderBooks(booksJson))
 }
 
 function renderBooks(json) {
